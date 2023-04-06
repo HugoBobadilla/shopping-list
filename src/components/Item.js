fc09e3e9e42e533b2bb55723173b28f9f3c040b0
HugoBobadilla <hugoabobadilla1998@gmail.com>
2023-04-06 16:54:08 -0400

    Delete edit button

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,35 +1,20 @@
 import React from 'react';
 import '../stylesheets/Item.css';
-import { TiDelete, TiEdit } from 'react-icons/ti';
+import { TiDelete } from 'react-icons/ti';
 
 const Item = ({ id, item, priority, isDone, setDone, removeItem }) => {
 
-  if(!isDone) {
-    return (
-      <div className='list-item-container'>
-        <div>
-          <p className='item-title' onClick={(id) => setDone(id)}>{item}</p>
-          <p className='item-priority'>Priority: <span>{priority}</span></p>
-        </div>
-        <div className="icons-container">
-          <TiEdit className='icon'/>
-          <TiDelete className='icon' onClick={(id) => removeItem(id)} />
-        </div>
+  return (
+    <div className={isDone ? 'list-item-container done' : 'list-item-container'}>
+      <div>
+        <p className='item-title' onClick={(id) => setDone(id)}>{item}</p>
+        <p className='item-priority'>Priority: <span>{priority}</span></p>
       </div>
-    );
-  } else {
-    return (
-      <div className='list-item-container done'>
-        <div>
-          <p className='item-title' onClick={(id) => setDone(id)}>{item}</p>
-          <p className='item-priority'>Priority: <span>{priority}</span></p>
-        </div>
-        <div className="icons-container">
-          <TiDelete className='icon' onClick={(id) => removeItem(id)} />
-        </div>
+      <div className="icons-container">
+        <TiDelete className='icon' onClick={(id) => removeItem(id)} />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Item;
